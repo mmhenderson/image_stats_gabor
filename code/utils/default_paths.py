b780244957975efd829668aa4f24ceeb953b90d7
Margaret Marie Henderson <mmhender@cmu.edu>
2022-06-03 11:47:01 -0400

    small changes to paths

--- code/utils/default_paths.py
+++ code/utils/default_paths.py
@@ -6,17 +6,20 @@ nsd_root = path_defs.nsd_path
 root = path_defs.root
 
 # Where we are keeping the preprocessed NSD stimuli/labeling data
-stim_root = os.path.join(root, 'nsd/stimuli/')    
-stim_labels_root = os.path.join(root, 'nsd/labels/')    
+stim_root = os.path.join(root, 'nsd','stimuli')    
+stim_labels_root = os.path.join(root, 'nsd','labels')    
 
 # Where to save model fits
-save_fits_path = os.path.join(root,'image_stats_gabor/model_fits/')
+save_fits_path = os.path.join(root,'image_stats_gabor','model_fits')
+
+# Where to save any figures we make
+fig_path = os.path.join(root, 'image_stats_gabor','figures')
 
 # Path where gabor model features will be saved
-gabor_texture_feat_path = os.path.join(root, 'features/gabor_texture/')
+gabor_texture_feat_path = os.path.join(root, 'features','gabor_texture')
 
 # Path where AlexNet features will be saved
-alexnet_feat_path = os.path.join(root, 'features/alexnet/')
+alexnet_feat_path = os.path.join(root, 'features','alexnet')
 
-# Where the raw beta weights are located
+# Where the raw NSD beta weights are located
 beta_root = os.path.join(nsd_root,'nsddata_betas','ppdata')
