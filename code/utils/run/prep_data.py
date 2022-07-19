416f5201a9625b1e728bb2ae34fa366aa42bf666
Margaret Marie Henderson <mmhender@cmu.edu>
2022-07-19 11:51:06 -0400

    update preproc scripts

--- code/utils/run/prep_data.py
+++ code/utils/run/prep_data.py
@@ -2,14 +2,14 @@ import os, sys, argparse
 
 from utils import nsd_utils
 from utils import default_paths
-from utils import coco_utils
+# from utils import coco_utils
 
 nsd_root = default_paths.nsd_root
 path_to_save = default_paths.stim_root
 print('nsd_root: %s'%nsd_root)
 print('path_to_save: %s'%path_to_save)
 
-if ~os.path.exists(path_to_save)
+if not os.path.exists(path_to_save):
     os.makedirs(path_to_save)
     
 if __name__ == '__main__':
@@ -20,12 +20,12 @@ if __name__ == '__main__':
     parser.add_argument("--debug", type=int,default=0)
    
     args = parser.parse_args()
-    
-    
+
+    print('debug=%d'%args.debug)
     
     nsd_utils.make_image_data_partitions(pct_holdout=0.10)
     
-    nsd_utils.get_subject_specific_images(nsd_root, path_to_save, npix=240)
+    nsd_utils.get_subject_specific_images(nsd_root, path_to_save, npix=240, debug=args.debug==1)
 
     coco_utils.get_coco_ids_indep(n_images=10000)
 
