d973c5274a84eb98408f51832141f371e38243d4
Margaret Marie Henderson <mmhender@cmu.edu>
2022-12-12 15:38:44 -0500

    updating utils

--- code/utils/default_paths.py
+++ code/utils/default_paths.py
@@ -7,12 +7,12 @@ import path_defs
 nsd_path = path_defs.nsd_path
 nsd_root = nsd_path 
 root = path_defs.root
-root_localnode = path_defs.root_localnode
 project_name = path_defs.project_name
 
 # Where we are keeping the preprocessed NSD stimuli/labeling data
 stim_root = os.path.join(root, 'nsd','stimuli')    
 stim_labels_root = os.path.join(root, 'nsd','labels')    
+nsd_rois_root = os.path.join(root, 'nsd', 'rois')
 
 # Where to save model fits
 save_fits_path = os.path.join(root, project_name, 'model_fits')
@@ -22,23 +22,18 @@ fig_path = os.path.join(root, project_name, 'figures')
 
 # Path where gabor model features will be saved
 gabor_texture_feat_path = os.path.join(root, 'features','gabor_texture')
-gabor_texture_feat_path_localnode = os.path.join(root_localnode, 'features','gabor_texture')
 
 # Path where texture model features will be saved
 pyramid_texture_feat_path = os.path.join(root, 'features', 'pyramid_texture')
-pyramid_texture_feat_path_localnode = os.path.join(root_localnode, 'features', 'pyramid_texture')
 
 # Path where sketch token features will be saved
 sketch_token_feat_path = os.path.join(root, 'features', 'sketch_tokens')
-sketch_token_feat_path_localnode = os.path.join(root_localnode, 'features', 'sketch_tokens')
 
 # Path where AlexNet features will be saved
 alexnet_feat_path = os.path.join(root, 'features','alexnet')
-alexnet_feat_path_localnode = os.path.join(root_localnode, 'features','alexnet')
 
 # Path where CLIP features will be saved
 clip_feat_path = os.path.join(root, 'features', 'CLIP')
-clip_feat_path_localnode = os.path.join(root_localnode, 'features', 'CLIP')
 
 # Where the raw NSD beta weights are located
 beta_root = os.path.join(nsd_root,'nsddata_betas','ppdata')
